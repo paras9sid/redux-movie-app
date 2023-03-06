5a60f4e04183c5ed1c835402632b78ad95005ead
paras9sid <sidharthjainp9@gmail.com>
2023-03-06 17:23:56 +0530

    Adding UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
+import { data } from "../data";
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Project Setup</h1>
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favorites</div>
+        </div>
+
+        <div className="list">
+          {/* grab the data from data.js and map over here */}
+          {data.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
