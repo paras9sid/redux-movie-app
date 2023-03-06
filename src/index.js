1a8579ae07a3aee7c7b8782a9d2e3691720d7dd7
paras9sid <sidharthjainp9@gmail.com>
2023-03-06 20:04:38 +0530

    redux installed , store created and its properties checked in console

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+//package imports
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+
+//file imports
 import App from "./components/App";
 import "./index.css";
+import movies from "./reducers";
+
+const store = createStore(movies);
+console.log("store", store); // will show the properties of store in object form when clicked in console
+console.log("STATE", store.getState()); // empty array State as defined inr educer will reflect
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-export default function movies(state = [], action) {
+export default function movies(state = {}, action) { // any change in state wheter state = [], or {} or "someString" or 1 will show in colse via indexjs root
   //state in store inititally empty array , and action trigger by UI
   if (action.type === "ADD_MOVIES") {
     return action.movies;
