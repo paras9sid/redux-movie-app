1bfb403f7902f73ba9a96dee02c65dcb16651431
paras9sid <sidharthjainp9@gmail.com>
2023-03-13 20:46:55 +0530

    Middleware Modified-arrow function-curried function

--- src/index.js
+++ src/index.js
@@ -8,14 +8,21 @@ import App from "./components/App";
 import "./index.css";
 import rootReducer from "./reducers";
 
-const logger = function ({ dispatch, getState }) {
-  return function (next) {
-    return function (action) {
-      console.log("ACTION TYPE = ", action.type);
+// const logger = function ({ dispatch, getState }) {
+//   return function (next) {
+//     return function (action) {
+//       console.log("ACTION TYPE = ", action.type);
+//       next(action);
+//     };
+//   };
+// };
+
+//above function can be written as -> with help of arrow functions - curried function logger
+const logger = ({dispatch,setState}) => (next) => (action)=>{
+     console.log("ACTION TYPE = ", action.type);
       next(action);
-    };
-  };
-};
+
+}
 
 const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("store", store); // will show the properties of store in object form when clicked in console
